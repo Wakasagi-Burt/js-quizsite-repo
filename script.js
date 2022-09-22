e328a956c5789a630c6c28761f0c806deaad675a
brett shulman <bshulman810@gmail.com>
2022-09-22 08:34:44 -0400

    mid push

---
+++ script.js
@@ -0,0 +1,43 @@
+var start = document.getElementById("start");
+var startBtn = document.getElementById("start-button");
+var quiz = document.getElementById("quiz");
+var timer = document.getElementById("timer");
+var questions = document.getElementById("questions");
+var answerSelec = document.getElementById("answer-selec");
+var quizEnd = document.getElementById("quiz-end");
+var scoreText = document.getElementById("score-text");
+var score = document.getElementById("score");
+var scoreSubmit = document.getElementById("score-submit");
+var playerInit = document.getElementById("player-initials");
+var submitBtn = document.getElementById("submit-button");
+
+var quizStart = document.querySelectorAll(".quiz-start");
+
+startBtn.addEventListener("click", function() {
+
+});
+
+submitBtn.addEventListener("click", function() {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
